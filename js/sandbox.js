86c552804416e889d6040809a0f820359570aa87
Octopus <octaviogodoyg@gmail.com>
2020-07-17 18:21:09 -0400

    envio de control flow

---
+++ js/sandbox.js
@@ -0,0 +1,17 @@
+//for loops
+
+// for(let i =0;i <5;i++){
+//     console.log('un loop ' + i);
+//     console.log('un loop ',  i);
+// }
+// console.log('loop finalizado...');
+
+// const names=['shaun','mario','luigi'];
+// for(let i =0;i<names.length; i++){
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}<div>`;
+//     console.log(html);
+// }
+
+// while loops
+
