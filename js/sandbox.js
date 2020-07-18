a44f3859a27929de193202989d8f257e1173ba5a
Octopus <octaviogodoyg@gmail.com>
2020-07-17 20:49:35 -0400

    envio switch control

--- js/sandbox.js
+++ js/sandbox.js
@@ -14,4 +14,75 @@
 // }
 
 // while loops
+// let i =5;
+// while(i<5){
+//     console.log('in while loop ' + i);
+//     i++;
+// }
 
+// do{
+//     console.log('in while loop ' + i);
+//     i++;
+// }while(i<5);
+
+// if statements
+// const age =20;
+// if (age>20) {
+//     console.log('you are over 20 year old');
+// }
+
+
+// const ninjas=['octavio','ryu','chun-li','mario'];
+// if (ninjas.length>4){
+//     console.log('that´s a lot de ninjas');
+// }
+
+// const password = 'p@ssword1234';
+// if (password.length>=12 && password.includes('@')) {
+//     console.log('that password is mighty strong');
+// }else if (password.length >=8 || password.includes('@') && password.length >=5) {
+//     console.log('that password is long enough')
+// }else{
+//     console.log('password is not long enough');
+// }
+
+
+
+// logical Notification(!)
+// let user = false;
+// if (user ) {
+//  console.log();   
+// }
+
+// break and continue
+
+// const scores = [50,25,0,30,100,20,10];
+
+// for(let i =0;i< scores.length;i++){
+
+//     if (scores[i]===0){
+//         continue;
+//     }
+//     console.log('congrats, you got the top score!', scores[i]);
+
+//     if (scores[i]===100){
+//         console.log('congrats, you got the top score!');
+//         break;
+//     }
+
+// }
+
+
+const grade = 'D';
+
+// switch statements
+
+if (grade==='A') {
+    
+} else if (grade==='B') { 
+} else if (grade==='C') { 
+} else if (grade==='D') { 
+} else if (grade==='E') { 
+} else {
+    
+}
