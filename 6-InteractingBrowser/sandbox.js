0628f0bfe1ae3d44d1eb2f510f30909307e80ca5
Octopus <octaviogodoyg@gmail.com>
2020-07-20 11:22:32 -0400

    envio del DOM

---
+++ 6-InteractingBrowser/sandbox.js
@@ -0,0 +1,12 @@
+// const para = document.querySelector('div.error');
+// console.log(para);
+
+// const para = document.querySelectorAll('p');
+// const para = document.querySelectorAll('.error');
+// console.log(para[2]);
+
+const para = document.getElementById('page-title');
+console.log(para);
+
+const error = document.getElementsByClassName('error');
+console.log(error);
